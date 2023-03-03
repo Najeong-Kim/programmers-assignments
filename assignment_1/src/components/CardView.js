8f219b02913e850ca753ed4796d443aaca161ec2
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2023-03-03 15:31:31 +0900

    homepage에 card 추가

--- assignment_1/src/components/CardView.js
+++ assignment_1/src/components/CardView.js
@@ -1,3 +1,5 @@
+import { cardPlane } from "./Card";
+
 class CardView {
   constructor($main) {
     this.$main = $main;
@@ -7,6 +9,16 @@ class CardView {
     const containerDiv = document.createElement("div");
     containerDiv.setAttribute("id", "cards_container");
     this.$main.appendChild(containerDiv);
+
+    const personalInfo = JSON.parse(localStorage.getItem("personalInfo"))
+
+    for(let i = 0; i < personalInfo.length; i++) {
+      const card = cardDiv(i)
+      card.appendChild(cardPlane("front", personalInfo[i].nickname))
+      card.appendChild(cardPlane("back", personalInfo[i].mbti))
+      containerDiv.appendChild(card)
+    }
+
   }
 }
 export default CardView;
