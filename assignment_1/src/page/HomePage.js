0b8d3c1616a4e05cd99d041fbc6f32caefe4a8b3
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2023-03-03 15:20:34 +0900

    인사 정보 페이지 타이틀 추가

--- assignment_1/src/page/HomePage.js
+++ assignment_1/src/page/HomePage.js
@@ -1,10 +1,14 @@
+import ContentTitle from "../components/ContentTitle";
+
+
 class HomePage {
   constructor($main) {
     this.$main = $main;
   }
 
   render() {
-
+    const title = new ContentTitle(this.$main, "Great People")
+    title.render();
   }
 }
 
