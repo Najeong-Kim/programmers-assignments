25f5e727dbe855f222ef143b2b7068b19bad045b
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2023-03-03 16:06:21 +0900

    SignupView에 요소 렌더

--- assignment_1/src/components/SignupView.js
+++ assignment_1/src/components/SignupView.js
@@ -11,6 +11,19 @@ class SignupView {
     const form = document.createElement("form")
     form.setAttribute("id", "grepp_form")
     containerDiv.appendChild(form)
+
+    input("text", "name", "이름");
+    input("email", "email", "이메일");
+    input("text", "nickname", "닉네임");
+
+    const roleValList = ["", "frontend", "backend", "fullstack"]
+    const roleTxtList = ["직군을 선택해주세요", "프론트엔드", "백엔드", "풀스택"]
+    const mbtiValList = ["", "ENFJ", "ENTJ", "ENFP", "ENTP", "ESFJ", "ESTJ", "ESFP", "ESTP", "INFJ", "INTJ", "INFP", "INTP", "ISFJ", "ISTJ", "ISFP", "ISTP"]
+    const mbtiTxtList = ["MBTI를 선택해주세요", "ENFJ", "ENTJ", "ENFP", "ENTP", "ESFJ", "ESTJ", "ESFP", "ESTP", "INFJ", "INTJ", "INFP", "INTP", "ISFJ", "ISTJ", "ISFP", "ISTP"]
+    select("role", roleValList, roleTxtList);
+    select("mbti", mbtiValList, mbtiTxtList);
+
+    button("submit", "등록");
 	}
 }
 export default SignupView
